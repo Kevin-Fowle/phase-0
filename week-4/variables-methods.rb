06e4d240ea4f4b11097710f6cd66750a132ede56
Kevin-Fowle <ksfowle@comcast.net>
2016-02-09 13:12:53 -0600

    4.3 final upload

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,35 @@
+puts 'Hello, what is your first name?'
+first_name=gets.chomp
+puts 'What about your middle name?'
+middle_name=gets.chomp
+puts 'Ok one more. What is your last name?'
+last_name=gets.chomp
+puts 'Nice to meet you ' +first_name+' '+middle_name+' '+last_name
+
+
+
+puts 'what is your favorite number'
+fav_number=gets.chomp
+puts 'Do you like it better now? It is bigger AND BETTER'
+puts fav_number.to_i + 1.to_i
+
+=begin
+https://github.com/Kevin-Fowle/phase-0/blob/master/week-4/address/my_solution.rb
+
+https://github.com/Kevin-Fowle/phase-0/blob/master/week-4/define-method/my_solution.rb
+
+https://github.com/Kevin-Fowle/phase-0/blob/master/week-4/math/my_solution.rb
+
+
+A local variable is a command in ruby that allows a developer to set a value to a particular command. You could store a string to myString and have it display all the text you assigned to myString just by calling that variable.
+
+Methods are a set commands in ruby that lets the programmer alter particular points of code. Methods include addition, subtraction and gets.chomp. These are all built into ruby. If you run gets.chomp it will get the input of the user. There are tons of ruby methods to use.
+
+A variable is a set and defined by the user. The methods that ruby has built into it cannot be defined by the user. Variables are used to store information for use later on. Methods are different ways to alter your code.
+
+IRB is a quick and easy way to run a ruby program from the command line. After you type in irb. You can start making your own ruby program on the command line...neat stuff
+
+On the command line, if you use rspec file-name.rb it will run rspec on the recently made program. rspec looks at your code to see if there are any syntax errors. If there are, it will alert you with a number of failures found.
+
+Ruby is quite the changeup from html and css. It requires me to look at problems in a whole different light. I think just switching over to a different mindset was a struggle. For the past few weeks I was learning moreover design. Now that things have switched, I need to examine the problem a little differently. Most of this stuff was review, but I made some silly errors along the way. I guess I just need more practice.
+=end
